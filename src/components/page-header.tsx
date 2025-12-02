d945446a257272b4b58d0edae26c2f940e9920d8
Net-Nutdanai04 <nutdanai.tharar@gmail.com>
2025-12-03 01:34:55 +0700

    ✨feat[breadcrumb]: create component

---
+++ src/components/page-header.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { House } from 'lucide-react';
+
+import Link from 'next/link';
+import React from 'react';
+
+// Define the shape of each breadcrumb item
+export interface BreadcrumbItemType {
+  label: string;
+  href?: string;
+  isPage?: boolean;
+}
+
+// Props for the reusable BaseHeader
+export interface BaseHeaderProps {
+  breadcrumbs: BreadcrumbItemType[];
+}
+
+export const PageHeader = ({ breadcrumbs }: BaseHeaderProps) => {
+  return (
+    <div className="mb-4 flex flex-col gap-2">
+      <div className="flex items-center gap-2">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild></BreadcrumbLink>
+              <Link href="/">
+                <House size={16} />
+              </Link>
+              <BreadcrumbSeparator />
+            </BreadcrumbItem>
+            {breadcrumbs.map((breadcrumb, index) => (
+              <BreadcrumbItem key={index}>
+                {breadcrumb.isPage ? (
+                  <BreadcrumbPage>{breadcrumb.label}</BreadcrumbPage>
+                ) : breadcrumb.href ? (
+                  <BreadcrumbLink asChild>
+                    <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+                  </BreadcrumbLink>
+                ) : (
+                  <span>{breadcrumb.label}</span>
+                )}
+                {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+              </BreadcrumbItem>
+            ))}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </div>
+  );
+};
