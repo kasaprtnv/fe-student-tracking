63caedb6090a31243b0fb8d688fc12e61c39e48c
IceKspRt <kasap.rtnv@gmail.com>
2025-12-12 22:48:51 +0700

    fix: edit default page and edit config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useTranslations } from 'next-intl';
-import Link from 'next/link';
-import { useUser } from '@/hooks/use-user';
-import { useAuth } from '@/hooks/use-auth';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  const t = useTranslations();
-  const { getUserProfile } = useUser();
-  const { user } = useAuth();
-
-  useEffect(() => {
-    if (!user) {
-      getUserProfile();
-    }
-  }, [user, getUserProfile]);
-
-  return (
-    <>
-      <h1>{t('homepage.title')}</h1>
-      <p>{t('homepage.welcomeMessage')}</p>
-      <div className="mt-4">
-        <Link href="/student">Go to Students</Link>
-      </div>
-    </>
-  );
+  redirect('/login');
 }
