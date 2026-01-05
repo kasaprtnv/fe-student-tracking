4729b05ffd76bf2224ba284b4b26fa0e04b404a8
shinathorn <shinathorn.s@gmail.com>
2026-01-05 20:41:26 +0700

    Fix : selectedmilestone,UI milestone
    Fix selected milestone , UI milestone page

--- src/validations/milestone.ts
+++ src/validations/milestone.ts
@@ -1,29 +1,58 @@
 import { z } from 'zod';
 
 export const createMilestoneSchema = (t: (key: string) => string) =>
-  z.object({
-    name: z.string().nonempty(t('errors.name-required')),
-    description: z.string().optional(),
-    notifyBeforeDays: z
-      .int(t('errors.notifyBeforeDays-integer'))
-      .min(0, t('errors.notifyBeforeDays-min')),
-    dayPeriod: z
-      .int(t('errors.dayPeriod-integer'))
-      .min(0, t('errors.dayPeriod-min')),
-  });
+  z
+    .object({
+      name: z.string().nonempty(t('errors.name-required')),
+      description: z.string().optional(),
+
+      notifyBeforeDays: z
+        .number()
+        .int(t('errors.notifyBeforeDays-integer'))
+        .min(0, t('errors.notifyBeforeDays-min')),
+
+      dayPeriod: z
+        .number()
+        .int(t('errors.dayPeriod-integer'))
+        .min(0, t('errors.dayPeriod-min')),
+    })
+    .superRefine((data, ctx) => {
+      if (data.notifyBeforeDays > data.dayPeriod) {
+        ctx.addIssue({
+          path: ['notifyBeforeDays'],
+          code: z.ZodIssueCode.custom,
+          message: t('errors.notifyBeforeDays-greater-than-dayPeriod'),
+        });
+      }
+    });
 
 export const updateMilestoneSchema = (t: (key: string) => string) =>
-  z.object({
-    name: z.string().nonempty(t('errors.name-required')),
-    description: z.string().optional(),
-    notifyBeforeDays: z
-      .int(t('errors.notifyBeforeDays-integer'))
-      .min(0, t('errors.notifyBeforeDays-min')),
-    dayPeriod: z
-      .int(t('errors.dayPeriod-integer'))
-      .min(0, t('errors.dayPeriod-min')),
-    isActive: z.boolean().optional(),
-  });
+  z
+    .object({
+      name: z.string().nonempty(t('errors.name-required')),
+      description: z.string().optional(),
+
+      notifyBeforeDays: z
+        .number()
+        .int(t('errors.notifyBeforeDays-integer'))
+        .min(0, t('errors.notifyBeforeDays-min')),
+
+      dayPeriod: z
+        .number()
+        .int(t('errors.dayPeriod-integer'))
+        .min(0, t('errors.dayPeriod-min')),
+
+      isActive: z.boolean().optional(),
+    })
+    .superRefine((data, ctx) => {
+      if (data.notifyBeforeDays > data.dayPeriod) {
+        ctx.addIssue({
+          path: ['notifyBeforeDays'],
+          code: z.ZodIssueCode.custom,
+          message: t('errors.notifyBeforeDays-greater-than-dayPeriod'),
+        });
+      }
+    });
 
 export type CreateMilestoneFormData = z.infer<
   ReturnType<typeof createMilestoneSchema>
