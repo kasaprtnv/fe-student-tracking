216b2a471997eba1cb3c7330040013bb858ee4e3
IceKspRt <kasap.rtnv@gmail.com>
2026-02-27 18:11:23 +0700

    fix(personal information) : [179] change word and add description for upload profile

--- src/components/profile/upload-profile-dialog.tsx
+++ src/components/profile/upload-profile-dialog.tsx
@@ -3,6 +3,7 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -81,6 +82,7 @@ export const UploadProfileDialog = ({
       <DialogContent className="md:max-w-[750px]">
         <DialogHeader>
           <DialogTitle>แก้ไขรูปโปรไฟล์</DialogTitle>
+          <DialogDescription>เลือกรูปภาพที่ต้องการใช้</DialogDescription>
         </DialogHeader>
         <div
           {...getRootProps()}
@@ -118,8 +120,8 @@ export const UploadProfileDialog = ({
               <>
                 <div className="mb-1 text-center text-lg">
                   {isDragActive
-                    ? 'ปล่อยไฟล์ที่นี่...'
-                    : 'ลากและปล่อยไฟล์ หรือคลิกเพื่อเลือกไฟล์'}
+                    ? 'วางไฟล์ที่นี่...'
+                    : 'ลากไฟล์มาวางที่นี่ หรือคลิกเพื่อเลือกไฟล์'}
                 </div>
                 <div className="text-center text-sm text-gray-600">
                   รองรับไฟล์ PNG, JPG, JPEG (ขนาดไม่เกิน 5MB)
